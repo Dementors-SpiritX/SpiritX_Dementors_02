34505dab29092e2eeb665d7caa0b65db07bdfd8f
UdasriHasindu <usrimart@gmail.com>
2025-03-08 16:02:38 +0530

    add signup actions

---
+++ src/lib/signupActions.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { redirect } from 'next/navigation';
+
+import { createClient } from '@/lib/supabaseServer';
+
+export async function signup(formData: FormData) {
+	const supabase = await createClient();
+
+	// type-casting here for convenience
+	const data = {
+		email: formData.get('email') as string,
+		password: formData.get('password') as string,
+		phone: formData.get('phone') as string,
+		username: formData.get('username') as string,
+	};
+
+	// Handle missing fields properly
+	if (!data.username || !data.email || !data.phone || !data.password) {
+		redirect('/auth/error');
+	}
+
+	const { error } = await supabase.auth.signUp(data);
+
+	if (error) {
+		redirect('/auth/error');
+	}
+
+	await supabase.from('users').insert(data);
+
+	// return success instead of redirecting
+	return { success: true, message: 'Invitation sent to user email.' };
+}
